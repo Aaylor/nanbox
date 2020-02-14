78e4c7cb67271b406e2a772f4cf67887af604e3b
Guillaume <guillaume.cluzel@ens-lyon.fr>
2020-02-14 22:09:19 +0100

    remove while

--- shortstring_demo.c
+++ shortstring_demo.c
@@ -4,21 +4,21 @@
 
 int main() {
 	printf("Enter short strings of up to 6 chars to dump, q to quit.\n");
-	while (1) {
+	// while (1) {
 		char buf[7] = "azerty";
 		size_t len;
 		printf("Short string --> ");
 		// if (scanf("%6s", buf) == EOF)
 		//	break;
-		if (!strcmp(buf, "q"))
-			break;
+		// if (!strcmp(buf, "q"))
+		// 	break;
 		len = strlen(buf);
 		nanbox_t val = nanbox_shortstring_create(buf, len);
 		printf("%p \"%.6s\" (length %d)\n",
 		       (void*)val.as_int64,
 		       nanbox_shortstring_chars(&val),
 		       nanbox_shortstring_length(val));
-	}
+	// }
 	printf("\n");
 	return 0;
 }
